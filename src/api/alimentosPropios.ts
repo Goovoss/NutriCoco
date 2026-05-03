2b9293d0dee6e34b8dd88437d1e77ca054c81ff2
Goovoss <vergaraydaniel1@gmail.com>
2026-05-03 20:07:46 +0200

    cambio en la url de la API

--- src/api/alimentosPropios.ts
+++ src/api/alimentosPropios.ts
@@ -1,6 +1,6 @@
 import type { Ingrediente } from "../types";
 
-const BASE_URL = "http://localhost:3001/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL + "/api/v1";
 
 interface AlimentoAPI {
   id: string;
